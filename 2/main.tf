205d9d04210010fa079c237f7299405093dbcb07
kookoohs <kookoo7164@gmail.com>
2024-09-26 11:21:09 +0900

    First commit

---
+++ 2/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "172.16.8.0/24"
+  tags = {
+    Name = "simple-vpc-stu8"
+  }
+}
